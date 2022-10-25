bec571a8e42653c8c01cb28b105129f73df9076a
Amaywe <117047261+Amaywe@users.noreply.github.com>
2022-10-25 14:22:12 +0200

    Initial Commit

---
+++ AFP4/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  AFP4
+//
+//  Created by Klesya Loha on 25/10/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.pink)
+            Text("Hello Klesyaa!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
